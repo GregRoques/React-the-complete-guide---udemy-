df47fd87afafc0a986f074f5473bf89ee6681f48
Greg Roques <greg.roques@gmail.com>
2019-03-25 15:13:03 -0400

    React refresher

--- section3/src/App.js
+++ section3/src/App.js
@@ -2,24 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Cat from './Containers/Cat';
+
 class App extends Component {
+
+  state = {
+    cats: [
+      {name: 'Nacheaux', age: 5},
+      {name: 'Callie', age: 11},
+      {name: 'Midnight', age: 10}
+    ],
+    otherState: `Still Lil' Nacheaux but the dividends not little.`
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      cats: [
+        {name: 'Piper', age: 2},
+        {name: 'Sugar', age: 0.25},
+        {name: 'Spice', age: 0.25},
+      ]
+    })
+  }
+
+nameChangedHandler = (event)=> {
+  this.setState({
+    cats: [
+    {name: 'Piper', age: 2},
+    {name: event.target.value, age: 0.25},
+    {name: 'Spice', age: 0.25},
+    ]
+  })
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Cat
+        name={this.state.cats[0].name}
+        age={this.state.cats[0].age}/>
+
+        <Cat
+        name={this.state.cats[1].name}
+        age={this.state.cats[1].age}
+        click={this.switchNameHandler.bind(this, 'Meow!!!!')}
+        changed={this.nameChangedHandler}
+        >I like scratching furniture and eating treats.</Cat>
+
+         <Cat
+         name={this.state.cats[0].name}
+         age={this.state.cats[0].age}/>
+
       </div>
     );
   }

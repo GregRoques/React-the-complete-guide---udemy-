cf2388b8afe81f5cf3bd92a19c5571df81524839
Greg Roques <greg.roques@gmail.com>
2019-03-25 20:02:26 -0400

    short exercise

--- section3/src/App.js
+++ section3/src/App.js
@@ -1,77 +1,113 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserInput from './Presentational/UserInput';
+import UserOutput from './Presentational/UserOutput';
 
-import Cat from './Presentational/Cat';
-
-class App extends Component {
+class App extends Component{
 
   state = {
-    cats: [
-      {name: 'Nacheaux', age: 5},
-      {name: 'Callie', age: 11},
-      {name: 'Midnight', age: 10}
-    ],
-    otherState: `Still Lil' Nacheaux but the dividends not little.`
+    name: 'Paul'
   }
 
-  switchNameHandler = (newName) => {
+  eventHandler = (event) =>{
     this.setState({
-      cats: [
-        {name: 'Piper', age: 2},
-        {name: 'Sugar', age: 0.25},
-        {name: 'Spice', age: 0.25},
-      ]
+      name: event.target.value
     })
   }
 
-nameChangedHandler = (event)=> {
-  this.setState({
-    cats: [
-    {name: 'Piper', age: 2},
-    {name: event.target.value, age: 0.25},
-    {name: 'Spice', age: 0.25},
-    ]
-  })
-}
+  render(){
+    return(
+      <div>
+        <UserInput
+        name={this.state.name}
+        changed={this.eventHandler}
+        />
 
-  render() {
-
-    const style = {
-      backgroundColor: 'white',
-      font: 'sans-serif',
-      border: '1px solid blue',
-      padding: '8px',
-      marginTop: '10px',
-      borderRadius: '15px 15px',
-      cursor: 'pointer',
-      fontWeight: 'bold',
-      color:'red'
-    };
-
-    return (
-      <div className="App">
-        <button 
-          style ={style}
-          onClick={this.switchNameHandler}>Switch Name</button>
-        <Cat
-          name={this.state.cats[0].name}
-          age={this.state.cats[0].age}/>
-
-        <Cat
-          name={this.state.cats[1].name}
-          age={this.state.cats[1].age}
-          click={this.switchNameHandler.bind(this, 'Meow!!!!')}
-          changed={this.nameChangedHandler}
-          >I like scratching furniture and eating treats.
-        </Cat>
-
-         <Cat
-          name={this.state.cats[0].name}
-          age={this.state.cats[0].age}/>
+        <UserOutput
+        name={this.state.name}
+        />
       </div>
-    );
+    )
   }
+
+
+
 }
 
 export default App;
+
+
+// ====================================================== Cat Exercise
+
+// import Cat from './Presentational/Cat';
+
+// class App extends Component {
+
+//   state = {
+//     cats: [
+//       {name: 'Nacheaux', age: 5},
+//       {name: 'Callie', age: 11},
+//       {name: 'Midnight', age: 10}
+//     ],
+//     otherState: `Still Lil' Nacheaux but the dividends not little.`
+//   }
+
+//   switchNameHandler = (newName) => {
+//     this.setState({
+//       cats: [
+//         {name: 'Piper', age: 2},
+//         {name: 'Sugar', age: 0.25},
+//         {name: 'Spice', age: 0.25},
+//       ]
+//     })
+//   }
+
+// nameChangedHandler = (event)=> {
+//   this.setState({
+//     cats: [
+//     {name: 'Piper', age: 2},
+//     {name: event.target.value, age: 0.25},
+//     {name: 'Spice', age: 0.25},
+//     ]
+//   })
+// }
+
+//   render() {
+
+//     const style = {
+//       backgroundColor: 'white',
+//       font: 'sans-serif',
+//       border: '1px solid blue',
+//       padding: '8px',
+//       marginTop: '10px',
+//       borderRadius: '15px 15px',
+//       cursor: 'pointer',
+//       fontWeight: 'bold',
+//       color:'red'
+//     };
+
+//     return (
+//       <div className="App">
+//         <button 
+//           style ={style}
+//           onClick={this.switchNameHandler}>Switch Name</button>
+//         <Cat
+//           name={this.state.cats[0].name}
+//           age={this.state.cats[0].age}/>
+
+//         <Cat
+//           name={this.state.cats[1].name}
+//           age={this.state.cats[1].age}
+//           click={this.switchNameHandler.bind(this, 'Meow!!!!')}
+//           changed={this.nameChangedHandler}
+//           >I like scratching furniture and eating treats.
+//         </Cat>
+
+//          <Cat
+//           name={this.state.cats[0].name}
+//           age={this.state.cats[0].age}/>
+//       </div>
+//     );
+//   }
+// }
